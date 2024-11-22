6b17aab8600892d412b8597ba9776edfe8a04cf0
Oskar Petr <oskar.petr@outlook.com>
2024-11-22 12:06:44 +0100

    Modul A 2

--- CzechSkills2024.Frontend/next.config.ts
+++ CzechSkills2024.Frontend/next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "gematsu.com",
       },
+      {
+        protocol: "https",
+        hostname: "",
+      },
     ],
   },
 };
